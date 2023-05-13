d176d5bdf2bb5215edd752b01c719b51f54d5a70
Jonathanzhao02 <zhaojonathan99@gmail.com>
2023-05-13 03:58:40 +0000

    Added additional barcode tests

---
+++ fuzz/fuzz_targets/code39.rs
@@ -0,0 +1,20 @@
+#![no_main]
+use std::cmp::min;
+use libfuzzer_sys::fuzz_target;
+use barcoders::sym::code11::Code11;
+
+const CHARS: [&str; 43] = [
+    "0", "1", "2", "3", "4", "5", "6", "7", "8", "9",
+    "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M",
+    "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",
+    "-", ".", " ", "$", "/", "+", "%",
+];
+
+fuzz_target!(|input: Vec<usize>| {
+    if input.len() > 0 {
+        let mut data: Vec<_> = input.iter().map(|x| CHARS[x % CHARS.len()]).collect();
+        data = data[..min(256, data.len())].to_vec();
+        let data = data.join("");
+        let _ = Code11::new(data).unwrap();
+    }
+});
