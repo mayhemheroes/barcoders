a9efd421f9299f7bab97aa85ca3737330b456149
Jonathanzhao02 <zhaojonathan99@gmail.com>
2023-05-13 04:05:10 +0000

    Fixed Code39 test

--- fuzz/fuzz_targets/code39.rs
+++ fuzz/fuzz_targets/code39.rs
@@ -1,7 +1,7 @@
 #![no_main]
 use std::cmp::min;
 use libfuzzer_sys::fuzz_target;
-use barcoders::sym::code11::Code11;
+use barcoders::sym::code39::Code39;
 
 const CHARS: [&str; 43] = [
     "0", "1", "2", "3", "4", "5", "6", "7", "8", "9",
@@ -15,6 +15,6 @@ fuzz_target!(|input: Vec<usize>| {
         let mut data: Vec<_> = input.iter().map(|x| CHARS[x % CHARS.len()]).collect();
         data = data[..min(256, data.len())].to_vec();
         let data = data.join("");
-        let _ = Code11::new(data).unwrap();
+        let _ = Code39::new(data).unwrap();
     }
 });
